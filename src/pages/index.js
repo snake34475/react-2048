bed18c7ac6fb1d3a0a8131a9d62d91e491499cf6
wangtengyao <86810637+snake34475@users.noreply.github.com>
2023-08-30 10:24:11 +0800

    提交页面

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,174 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import {useEffect, useState} from "react"
+// import VConsole from 'vconsole';
+const inter = Inter({subsets: ['latin']})
+import * as _ from 'lodash'
 
-const inter = Inter({ subsets: ['latin'] })
-
+const initArrAll = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    const [arrAll, setArrAll] = useState(initArrAll)
+    useEffect(() => {
+        // console.log("arrAll", arrAll)
+        window.addEventListener("keydown", handleKeyDown)
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown)
+        }
+    }, [arrAll])
+
+    useEffect(() => {
+        initArr()
+        // const vConsole = new VConsole();
+    }, [])
+
+    //键盘控制
+    const handleKeyDown = (e) => {
+        //右边
+        if (e.key === 'ArrowRight') {
+            console.log("右边")
+            //克隆
+            let _arrAll = _.cloneDeep(arrAll)
+            //反转到可执行方向
+            _arrAll = _arrAll.map(arr => arr.reverse())
+            //移动
+            _arrAll = MoveAndMerge(_arrAll)
+            //新增一个
+            _arrAll=MathAdd(_arrAll)
+            console.log("_arrAll",_arrAll)
+            //翻转回正常
+            _arrAll = _arrAll.map(arr => arr.reverse())
+            setArrAll(_arrAll)
+        }
+        //左边
+        if (e.key === 'ArrowLeft') {
+            console.log("左边")
+            let _arrAll = _.cloneDeep(arrAll)
+
+            _arrAll = MoveAndMerge(_arrAll)
+            _arrAll=MathAdd(_arrAll)
+            setArrAll(_arrAll)
+        }
+        //上边
+        if (e.key === 'ArrowUp') {
+            console.log("上边")
+            let _arrAll = _.cloneDeep(arrAll)
+            _arrAll=xyTransforYx(_arrAll)
+            _arrAll=MoveAndMerge(_arrAll)
+            _arrAll=MathAdd(_arrAll)
+            _arrAll=xyTransforYx(_arrAll)
+            setArrAll(_arrAll)
+        }
+        //下边
+        if (e.key === 'ArrowDown') {
+            console.log("下边")
+            let _arrAll = _.cloneDeep(arrAll)
+            _arrAll=xyTransforYx(_arrAll)
+            _arrAll = _arrAll.map(arr => arr.reverse())
+            _arrAll=MoveAndMerge(_arrAll)
+            _arrAll = _arrAll.map(arr => arr.reverse())
+            _arrAll=MathAdd(_arrAll)
+            _arrAll=xyTransforYx(_arrAll)
+            setArrAll(_arrAll)
+        }
+    }
+
+    //初始化坐标
+    const initArr = () => {
+        const x1 = Math.floor(Math.random() * 4)
+        const y1 = Math.floor(Math.random() * 4)
+        const x2 = Math.floor(Math.random() * 4)
+        const y2 = Math.floor(Math.random() * 4)
+        let _arrAll = _.cloneDeep(arrAll)
+        _arrAll[x1][y1] = 2
+        _arrAll[x2][y2] = 2
+        setArrAll(_arrAll)
+    }
+
+    //移动和合并
+    const MoveAndMerge = (arrAll) => {
+        return arrAll.map((arr, index) => {
+            let _arr = arr.filter(item => item !== 0)
+            //合并
+            for (let i = 0; i < _arr.length; i++) {
+                if (_arr[i] === _arr[i + 1]) {
+                    _arr[i] = _arr[i] * 2
+                    _arr[i + 1] = 0
+                }
+            }
+            _arr = _arr.filter(item => item !== 0)
+            const lg = arr.length - _arr.length
+            if (lg > 0) {
+                for (let i = 0; i < lg; i++) {
+                    _arr.push(0)
+                }
+            }
+            return _arr
+        })
+    }
+    //xy颠倒
+    const xyTransforYx=(arr)=>{
+        let _arr= [[],[],[],[]]
+        for (let x = 0; x < arr.length; x++) {
+            for (let y = 0; y < arr[x].length; y++) {
+                _arr[x][y]=arr[y][x]
+            }
+        }
+        return _arr
+    }
+    //随机添加
+    const MathAdd = (arr) => {
+        let _arr= arr
+        let endArr=_arr.map(item=>item[3])
+        let MathArr=[]
+        endArr.forEach((item,index)=>{
+            if(item===0){
+                MathArr.push(index)
+            }
+        })
+        let index=Math.floor(Math.random()*MathArr.length)
+        _arr[index][3]=2
+        // _arr=_arr[index][3]
+        return _arr
+    }
+    //基础生成
+    const Cell = (props) => {
+        return <div
+            className='w-24 h-24  text-center bg-amber-700 border-2 rounded-2xl flex items-center justify-center'>
+            <p>{props.val || 0}</p>
         </div>
-      </div>
+    }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const FloatCell = () => {
+        return <>
+            {arrAll.map((item, index) => {
+                return item.map((ite, inde) => <Cell key={inde} val={ite}></Cell>)
+            })}
+        </>
+    }
+    const KeyBoard = () => {
+        return <div className='w-96 text-center'>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <button className='h-16 w-16 border' onClick={()=>handleKeyDown({key:'ArrowUp'})}>上</button>
+            <div>
+                <button className='h-16 w-16 border' onClick={()=>handleKeyDown({key:'ArrowLeft'})}>左</button>
+                <button className='h-16 w-16 border' onClick={()=>handleKeyDown({key:'ArrowDown'})}>下</button>
+                <button className='h-16 w-16 border' onClick={()=>handleKeyDown({key:'ArrowRight'})}>右</button>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+    }
+    return (
+        <main>
+            {/*<h1>2048</h1>*/}
+            <div className="w-96 h-96 bg-amber-100 flex flex-wrap">
+                <FloatCell></FloatCell>
+            </div>
+            <KeyBoard></KeyBoard>
+        </main>
+    )
 }
